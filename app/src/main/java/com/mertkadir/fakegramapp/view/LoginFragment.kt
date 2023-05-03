43245803c9dacd7260db2eb557d6a6f5d38f3db3
Mert Kadir <mertkadirarslan@gmail.com>
2023-05-03 17:04:55 +0300

    Fragment and Navigation implements
    Create Fragment and Navigation
    use. firebase auth use and user creating
    user login

---
+++ app/src/main/java/com/mertkadir/fakegramapp/view/LoginFragment.kt
@@ -0,0 +1,66 @@
+package com.mertkadir.fakegramapp.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.mertkadir.fakegramapp.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.loginbutton.setOnClickListener {
+            val emilText = binding.editTextEmailLogin.text.toString()
+            val passwordText = binding.editTextPasswordLogin.text.toString()
+
+
+            auth.signInWithEmailAndPassword(emilText,passwordText).addOnSuccessListener {
+
+                val intent = Intent(view.context,MainActivity::class.java)
+                startActivity(intent)
+                requireActivity().finish()
+
+            }.addOnFailureListener {
+                Toast.makeText(view.context, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        binding.register.setOnClickListener {
+            val intent = Intent(it.context,RegisterActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+
+}
--- app/src/main/java/com/mertkadir/fakegramapp/view/RegisterActivity.kt
+++ app/src/main/java/com/mertkadir/fakegramapp/view/RegisterActivity.kt
@@ -1,18 +1,68 @@
 package com.mertkadir.fakegramapp.view
 
+import android.content.Intent
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.mertkadir.fakegramapp.R
 import com.mertkadir.fakegramapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityRegisterBinding
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        //Init
+        auth = Firebase.auth
+
+        binding.fragmentContainerView.visibility = View.GONE
+        binding.textView2.visibility = View.VISIBLE
+
     }
+
+    fun registerBTN(view: View){
+
+
+        val emailText = binding.editTextEmail.text.toString()
+        val passwordText = binding.editTextPassword.text.toString()
+        val passwordVerifyText = binding.editTextPasswordVerify.text.toString()
+
+        if (emailText.equals("") || passwordText.equals("") ||passwordVerifyText.equals("")){
+            Toast.makeText(this, "Enter Email and Password-passVerify", Toast.LENGTH_SHORT).show()
+        }else if (passwordText != passwordVerifyText){
+            Toast.makeText(this, "password do not match", Toast.LENGTH_SHORT).show()
+        }else {
+
+            auth.createUserWithEmailAndPassword(emailText,passwordText).addOnSuccessListener {
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }.addOnFailureListener {
+                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+
+    fun login(view: View){
+
+        binding.fragmentContainerView.visibility = View.VISIBLE
+        binding.registerLinearLayout.visibility = View.GONE
+        binding.textView2.visibility = View.GONE
+
+    }
+
 }
